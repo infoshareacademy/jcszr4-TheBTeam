3fbb17e6e87a6ba6d8b1b492391de3c0f80dd146
Mateusz Puchalski <mateusz.puchalski@gmail.com>
2021-10-03 20:48:35 +0200

    TheBTeam solution added

---
+++ TheBTeam.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TheBTeam.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
