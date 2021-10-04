46eca8f453c635ecd25a6193ff1c11d4f8dda351
Mariusz Malec <mario106c@gmail.com>
2021-10-04 23:29:39 +0200

    Remove Person.cs class to Logic

--- TheBTeam.BLL/Person.cs
+++ TheBTeam.BLL/Person.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace TheBTeam.ConsoleApp
+namespace TheBTeam.BLL
 {
     public class Person
     {
@@ -18,6 +22,5 @@ namespace TheBTeam.ConsoleApp
         public string phone { get; set; }
         public string address { get; set; }
         public string registered { get; set; }
-
     }
 }
--- TheBTeam.ConsoleApp/Program.cs
+++ TheBTeam.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using TheBTeam.BLL;
 
 namespace TheBTeam.ConsoleApp
 {
