b9a274f3d04edede74d010f67f95dea04cc61ef3
Michal Michanski <michamichapre@gmail.com>
2021-10-07 20:59:28 +0200

    Dodano metode ReadUserFile w Program.cs, Uporzadkowano pola w klasie User by pasowaly do Jsona.

--- TheBTeam.BLL/User.cs
+++ TheBTeam.BLL/User.cs
@@ -1,19 +1,21 @@
-﻿namespace TheBTeam.BLL
+﻿
+namespace TheBTeam.BLL
 {
     public class User
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string ID { get; set; }
+        public string Id { get; set; }
         public bool isActive { get; set; }
-        public decimal balance { get; set; }
+        public string balance { get; set; }
         public string currency { get; set; }
         public int age { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
         public string gender { get; set; }
         public string company { get; set; }
         public string email { get; set; }
-        private string phone { get; set; }
-        private string address { get; set; }
-        private string registered { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string registered { get; set; }
     }
+
 }
--- TheBTeam.ConsoleApp/Program.cs
+++ TheBTeam.ConsoleApp/Program.cs
@@ -1,18 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 using TheBTeam.BLL;
 
 namespace TheBTeam.ConsoleApp
 {
     class Program
     {
+
+        static List<User> ReadUserFile()
+        {
+            string fileName = "users.json";
+            string jsonstring = File.ReadAllText(fileName);
+            var userData = JsonSerializer.Deserialize<List<User>>(jsonstring);
+            return userData;
+        }
+
         static void Main(string[] args)
         {
             Menu menu = new Menu();
             User user = new User();
             Category category = new Category();
+            List<User> userDataList = ReadUserFile();
+
             do
             {
-                Console.WriteLine($"Welcome {user.FirstName} in the financial planner");
+                Console.WriteLine($"Welcome {user.firstName} in the financial planner");
                 int selectedOption = menu.SelectMenuOption();
                 Console.WriteLine("------------------------------------------");
                 switch (selectedOption)
@@ -23,10 +37,10 @@ namespace TheBTeam.ConsoleApp
                     case 2:
                         Console.WriteLine($"Add new user");
                         Console.WriteLine($"Enter FirstName");
-                        user.FirstName = Console.ReadLine();
+                        user.firstName = Console.ReadLine();
                         Console.WriteLine($"Enter LastName");
-                        user.LastName = Console.ReadLine();
-                        Console.WriteLine($"Welcome {user.FirstName} {user.LastName}");
+                        user.lastName = Console.ReadLine();
+                        Console.WriteLine($"Welcome {user.firstName} {user.lastName}");
                         break;
                     case 3:
                         Console.WriteLine($"Enter transation");
