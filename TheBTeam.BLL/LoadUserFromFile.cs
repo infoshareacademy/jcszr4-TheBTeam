cea8030d5dd8de3fcddcffc81cdd8ab1ea134739
Michal Michanski <michamichapre@gmail.com>
2021-10-15 17:18:05 +0200

    Added LoadUsedFromFile, User classes, added sourcefiles for User

---
+++ TheBTeam.BLL/LoadUserFromFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TheBTeam.BLL
+{
+    public class LoadUserFromFile
+    {
+        public static List<User> ReadUserFile()
+        {
+            string fileName = "users.json";
+            string jsonstring = File.ReadAllText(fileName);
+            List<User> userData = JsonConvert.DeserializeObject<List<User>>(jsonstring);
+            
+            return userData;
+        }
+
+    }
+}
