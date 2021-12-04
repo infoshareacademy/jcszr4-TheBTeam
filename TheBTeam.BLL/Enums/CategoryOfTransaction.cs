0dab4d204ff3ffc80591348da82bc83260d33164
Mateusz Puchalski <mateusz.puchalski@gmail.com>
2021-12-04 18:39:56 +0100

    Added feature of limiting category display in dropdown list

--- TheBTeam.BLL/Enums/CategoryOfTransaction.cs
+++ TheBTeam.BLL/Enums/CategoryOfTransaction.cs
@@ -1,26 +1,45 @@
-﻿namespace TheBTeam.BLL
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBTeam.BLL
 {
     public enum CategoryOfTransaction
     {
         All = 0,
-        Salary,
-        Prize,
-        ExtraMoney,
+        [Display(Name = "Salary")]
+        incomeSalary,
+        [Display(Name = "Prize")]
+        incomePrize,
+        [Display(Name = "Extra money")]
+        incomeExtraMoney,
 
-        Home,
-        Car,
-        School,
-        Kids,
-        Commute,
-        Food,
-        EatingOut,
-        Entertainment,
-        Medicine,
-        Clothing,
-        Special,
-        Other,
-        Credit,
-        LivingCharges
+        [Display(Name = "Home")]
+        outcomeHome = 100,
+        [Display(Name = "Car")]
+        outcomeCar,
+        [Display(Name = "School")]
+        outcomeSchool,
+        [Display(Name = "Kids")]
+        outcomeKids,
+        [Display(Name = "Commute")]
+        outcomeCommute,
+        [Display(Name = "Food")]
+        outcomeFood,
+        [Display(Name = "Eating out")]
+        outcomeEatingOut,
+        [Display(Name = "Entertainment")]
+        outcomeEntertainment,
+        [Display(Name = "Medicine")]
+        outcomeMedicine,
+        [Display(Name = "Clothing")]
+        outcomeClothing,
+        [Display(Name = "Special")]
+        outcomeSpecial,
+        [Display(Name = "Other")]
+        outcomeOther,
+        [Display(Name = "Credit")]
+        outcomeCredit,
+        [Display(Name = "Living charges")]
+        outcomeLivingCharges
         //TODO make to Categories for Income i Outcome separately
     }
 }
