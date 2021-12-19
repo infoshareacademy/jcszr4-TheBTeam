63f1f512b29deb20a4faa95d81f57a2a85db88b5
Mateusz Puchalski <mateusz.puchalski@gmail.com>
2021-12-19 19:09:16 +0100

    EFCore Nugets added

---
+++ TheBTeam.BLL/Models/CategoryBudget.cs
@@ -0,0 +1,12 @@
+﻿namespace TheBTeam.BLL.Models
+{
+    public class CategoryBudget
+    {
+        public decimal BudgetLeft;
+        public CategoryOfTransaction Category { get; set; }
+        public decimal PlanedBudget { get; set; }
+        public decimal ActualBudget { get; set; }
+
+
+    }
+}
