026bfab098f997d30e3500df53ad3ad2d78ea839
Mariusz Malec <mario106c@gmail.com>
2022-02-13 01:15:06 +0100

    fixed login view, register?

--- TheBTeam.Web/Controllers/AccountController.cs
+++ TheBTeam.Web/Controllers/AccountController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using TheBTeam.BLL.DAL;
+using TheBTeam.BLL.DAL.Entities;
 using TheBTeam.BLL.Models;
 using TheBTeam.BLL.Services;
 
@@ -14,10 +18,89 @@ namespace TheBTeam.Web.Controllers
     public class AccountController : Controller
     {
         private readonly IAccountService _accountService;
+        private readonly ILogger<AccountController> _logger;
+        private readonly PlannerContext _plannerContext;
 
-        public AccountController(IAccountService accountService)
+        public AccountController(IAccountService accountService, ILogger<AccountController> logger, PlannerContext plannerContext)
         {
             _accountService = accountService;
+            _logger = logger;
+            _plannerContext = plannerContext;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]//[HttpGet, AllowAnonymous]
+        public IActionResult Register()
+        {
+            RegisterDto model = new RegisterDto();
+            return View(model);
+        }
+
+        [HttpPost, AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterDto request)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: add scope
+                using var log = _logger.BeginScope("UserEmailCheckInRegistration");
+                _logger.LogDebug("Checking if user exists {userEmail}", request.Email);
+                //var userCheck = await _plannerContext.FindAsync(e=>e. request.Email);
+                
+                var userCheck = await _plannerContext.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+
+                if (userCheck == null)
+                {
+                    var user = new User
+                    {
+                        FirstName = "",
+                        LastName = "",
+                        Balance = 0,
+                        Currency = BLL.Currency.PLN,
+                        Age = 18,
+                        Gender = BLL.Gender.Genderless,
+                        Email = request.Email,
+                        CreatedAt = DateTime.Now,
+                        //TODO nadanie roli i hasla
+                        //Role = ""
+                        //Password = request.Password,
+                        //ConfirmPassword = request.ConfirmPassword
+                    };
+                    _logger.LogDebug("Creating new user");
+
+                    _plannerContext.Users.Add(user);
+                    await _plannerContext.SaveChangesAsync();
+                    //TODO validation here add
+                    _logger.LogDebug("User created successfully");
+                    return RedirectToAction("/");
+
+                    //TODO validate
+                    //var result = await _userManager.CreateAsync(user, request.Password);
+                    //if (result.Succeeded)
+                    //{
+                    //    _logger.LogDebug("User created successfully");
+                    //    return RedirectToAction("Login");
+                    //}
+                    //else
+                    //{
+                    //    if (result.Errors.Count() > 0)
+                    //    {
+                    //        foreach (var error in result.Errors)
+                    //        {
+                    //            ModelState.AddModelError("message", error.Description);
+                    //        }
+                    //    }
+                    //    return View(request);
+                    //}
+                }
+                else
+                {
+                    _logger.LogDebug("User with this email already exists");
+                    ModelState.AddModelError("message", "Email already exists.");
+                    return View(request);
+                }
+            }
+            return View(request);
+
         }
 
         [HttpGet]
@@ -46,7 +129,8 @@ namespace TheBTeam.Web.Controllers
 
                 if (Url.IsLocalUrl(returnUrl))
                 {
-                    return Redirect(returnUrl);
+                    //return Redirect(returnUrl);
+                    return RedirectToAction("Index", "Home", new { email = userName });
                 }
                 else
                 {
@@ -54,7 +138,7 @@ namespace TheBTeam.Web.Controllers
                 }
             }
 
-            return View();
+            return View("UserIsNotRegistered","Account");
         }
 
         private async Task<LoginResult> ValidateLogin(string userName, string password)
--- TheBTeam.Web/Controllers/HomeController.cs
+++ TheBTeam.Web/Controllers/HomeController.cs
@@ -27,9 +27,10 @@ namespace TheBTeam.Web.Controllers
             _userService = userService;
         }
 
-        [Authorize]
-        public async Task<IActionResult> Index()
+        [Authorize]//[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index(string email)
         {
+            ViewBag.Email = email;
             LoadDataFromFile.LoadUsersToDatbase(_plannerContext);
             var usersList = await _userService.GetAllUsers();
             return View(usersList);
