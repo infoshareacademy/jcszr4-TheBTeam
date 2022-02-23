d6f09a34b7c953140dca74d80fca821244769b1e
Mariusz Malec <mario106c@gmail.com>
2022-02-23 21:34:49 +0100

    added dbo.LogEvents and added redirect if you set wrong id for User methhods like /edit/detail/Delete

--- TheBTeam.Web/Program.cs
+++ TheBTeam.Web/Program.cs
@@ -3,8 +3,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Sinks.MSSqlServer;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,8 +27,23 @@ namespace TheBTeam.Web
                 .AddJsonFile("appsettings.json", false, true)
                 .Build();
 
+            var connectionString = "Server=localhost\\sqlexpress; Integrated Security=SSPI; Database=TheBTeam;";
+            var columnOptions = new ColumnOptions
+            {
+                AdditionalColumns = new Collection<SqlColumn>
+                {
+                    new SqlColumn
+                        {ColumnName = "Scope", PropertyName = "Scope", DataType = SqlDbType.NVarChar, DataLength = 64},
+                }
+            };
+
             Log.Logger = new LoggerConfiguration()
             .ReadFrom.Configuration(configuration)
+            .WriteTo.MSSqlServer(connectionString, sinkOptions: new MSSqlServerSinkOptions
+            {
+                 AutoCreateSqlTable = true,
+                 TableName = "LogEvents",
+            }, columnOptions: columnOptions).MinimumLevel.Warning().Enrich.FromLogContext()
             .CreateLogger();
 
             Log.Information("App starts ... ");
