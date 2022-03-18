d58806a78691133e84aacf1648ff1c5e7562d553
Michal Michanski <michamichapre@gmail.com>
2022-03-18 01:50:27 +0100

    Panel front and user partial added

---
+++ TheBTeam.Web/Controllers/AdminPanelController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using TheBTeam.BLL.DAL;
+using TheBTeam.BLL.DAL.Entities;
+using TheBTeam.BLL.Models;
+using TheBTeam.BLL.Services;
+
+namespace TheBTeam.Web.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly PlannerContext _plannerContext;
+        private UserService _userService;
+        private TransactionService _transactionService;
+        public AdminPanelController(PlannerContext plannerContext, ILogger<UserController> logger)
+        {
+            _plannerContext = plannerContext;
+            _userService = new UserService(plannerContext);
+            _transactionService = new TransactionService(plannerContext);
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AdminController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdminController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
